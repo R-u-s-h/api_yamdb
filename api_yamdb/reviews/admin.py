68efafceb6989d537ea5e2a1c2b9ae003ab7a7de
Ivan Reznichenko <ivreznic@gmail.com>
2022-08-10 16:08:23 +0300

    linters have been satisfied now
     - remove all unnesessary files
     - run `isort . ; black . ; flake8 .` to reformat project

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Comment, Genre, GenreTitle, Review, Title
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    ...
+
+
+class CommentAdmin(admin.ModelAdmin):
+    ...
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+class TitleAdmin(admin.ModelAdmin):
+    ...
+
+
+class GenreAdmin(admin.ModelAdmin):
+    ...
+
+
+class GenreTitleAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(GenreTitle, GenreTitleAdmin)
