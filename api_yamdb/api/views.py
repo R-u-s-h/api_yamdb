19e1e628bc90cea973587e77f70a44ef5997f7bf
HappyR0b0t <shishkinals@yandex.ru>
2022-08-10 23:56:23 +0700

    features/reviews/api. First commit

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets
+
+from api.serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer,
+)
+
+from reviews.models import Category, Genre, Title
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = GenreSerializer
+    queryset = Genre.objects.all()
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    serializer_class = TitleSerializer
+    queryset = Title.objects.all()
