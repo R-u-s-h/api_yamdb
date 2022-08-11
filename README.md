d485e2ab3d82fb233fa80a48d731a748a2f77ce7
Ivan Reznichenko <ivreznic@gmail.com>
2022-08-11 22:54:21 +0300

    csv import feature
     - custom django-admin command reviews/management/commands/import_csv.py
     - README.md description for this feature

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # api_yamdb
 api_yamdb
+
+## Наполнение базы данных
+Реализована возможность наполнения базы данных из .csv файлов.
+
+### Формат файлов
+Первая строка файла — имена полей модели.
+
+Остальные строки — значения полей.
+
+Разделитель — `,` (запятая).
+
+#### Пример файла:
+`static/data/category.csv`
+```csv
+id,name,slug
+1,Фильм,movie
+2,Книга,book
+3,Музыка,music
+```
+
+### Инструмент импорта данных
+Импорт данных реализован с помощью пользовательской команды `import_csv`
+скрипта `manage.py`
+
+#### Синтаксис
+```
+python manage.py import_csv <csv_file> <model_name>
+
+где:
+  csv_file              имя .csv файла
+  model_name            название модели в которую необходимо импортировать
+
+для увеличения уровня информативности можно использовать опцию:
+  -v2 (--verbosity 2)
+```
+
+#### Пример исползования
+```shell
+(venv) api_yamdb$ python manage.py import_csv static/data/category.csv Category -v2
+Importing module <module 'reviews.models' from '/Users/ivr/Git/yandex_practicum/api_yamdb/api_yamdb/reviews/models.py'>
+Using model <class 'reviews.models.Category'>
+Created instance <Category: movie>
+Updated instance <Category: book>
+Updated instance <Category: music>
+(venv) api_yamdb$
+```
---
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -0,0 +1,63 @@
+from csv import reader
+from importlib import import_module
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models.fields.related import RelatedField
+
+APP_MODELS = "reviews.models"
+
+
+class Command(BaseCommand):
+    help = "Import data from .csv file to model"
+
+    def add_arguments(self, parser):
+        parser.add_argument("csv_file", type=str, help="specify .csv file")
+        parser.add_argument(
+            "model_name", type=str, help="specify model name to import"
+        )
+
+    def handle(self, *args, **options):
+
+        model_name = options.get("model_name")
+
+        try:
+            module = import_module(APP_MODELS)
+        except ModuleNotFoundError:
+            raise CommandError(f"Modue '{APP_MODELS}' not found!")
+
+        if options.get("verbosity") > 1:
+            self.stdout.write(f"Importing module {module}")
+
+        try:
+            model = getattr(module, model_name)
+        except AttributeError:
+            raise CommandError(
+                f"Model '{model_name}' not found in module '{module}'!"
+            )
+
+        if options.get("verbosity") > 1:
+            self.stdout.write(f"Using model {model}")
+
+        related_fields = {
+            field.name
+            for field in model._meta.fields
+            if isinstance(field, RelatedField)
+        }
+
+        with open(options.get("csv_file")) as csv_file:
+            field_names: list[str] = list()
+            csv_reader = reader(csv_file)
+
+            for field_name in csv_reader.__next__():
+                if field_name in related_fields:
+                    field_name += "_id"
+                field_names.append(field_name)
+
+            for row in csv_reader:
+                fields = dict(zip(field_names, row))
+                instance, status = model.objects.update_or_create(**fields)
+                if options.get("verbosity") > 1:
+                    action = "Updated"
+                    if status:
+                        action = "Created"
+                    self.stdout.write(f"{action} instance {instance!r}")
