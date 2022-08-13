9b7b81373444419bc375024810b83abb287ac3a0
HappyR0b0t <shishkinals@yandex.ru>
2022-08-13 18:20:34 +0700

    Thrird commit. Main urls changes. Category searchfilter added

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,22 @@
+<<<<<<< Updated upstream
+=======
+from rest_framework import viewsets, filters
+
+>>>>>>> Stashed changes
 from api.serializers import (
     CategorySerializer,
     GenreSerializer,
     TitleSerializer,
 )
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 from reviews.models import Category, Genre, Title
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('slug',)
 
 
 class GenreViewSet(viewsets.ReadOnlyModelViewSet):
