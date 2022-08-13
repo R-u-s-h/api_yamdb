b7570e3489986c4002ffacad7a1d812e28ad67b0
HappyR0b0t <shishkinals@yandex.ru>
2022-08-13 23:45:27 +0700

    Fourth commit. Search filter added

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,7 +3,7 @@ from api.serializers import (
     GenreSerializer,
     TitleSerializer,
 )
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from reviews.models import Category, Genre, Title
 
 
@@ -15,6 +15,8 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
 class GenreViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = GenreSerializer
     queryset = Genre.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('slug',)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
